1ba896bfeaf209a92a07e1eeabfba6b80bd7697f
freecodeliuxing <liu1218xing01@163.com>
2016-08-03 16:59:56 +0800

    inti commit

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php echo "hello";
+	echo 23+4;
+	// echo <br>;
+	echo "bay";
+	// mysql_connect('127.0.0.1','liuxing','liu1218xing')?'niuren':'huairen';
+	?>
+</body>
+</html>
